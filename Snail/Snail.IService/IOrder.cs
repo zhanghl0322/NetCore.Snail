923f8138c3f2f20b5db59ea84ee403c1789d7e00
Allon <zhanghl0322@gmail.com>
2019-11-04 18:00:02 +0800

    蜗牛框架分层结构初始化

---
+++ Snail/Snail.IService/IOrder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snail.IService
+{
+    public interface IOrder
+    {
+
+    }
+}
---
+++ Snail/Snail.Model/BaseDBConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snail.Model
+{
+    /// <summary>
+    /// DB配置信息
+    /// </summary>
+    public class BaseDBConfig
+    {
+    }
+}
